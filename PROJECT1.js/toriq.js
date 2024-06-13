31f00c2ba8ce20fd889e7f4c603b420c99bea6bc
tro1q <160236320+tro1q@users.noreply.github.com>
2024-06-13 16:33:36 +0600

    Amazon clone
    This is amazon clone. Need to make improvement.

---
+++ PROJECT1.js/toriq.js
@@ -0,0 +1,50 @@
+//console.log("wllcome to my website!");
+//This part is a mini project 
+//const profile ={
+    //username: "@Toriqul",
+    //isFollow:false,
+    //followers:123,
+   // following:123,
+
+
+//console.log(typeof profile["username"])
+//this is operators part------- 
+//let a =5;
+//let b =2;
+//console.log("a+b=",a+b);
+//console.log("a++ =",++a);
+// pre (++a) ,,post (a++)
+//console.log("a =",a);
+//console.log("a%b=",a%b);
+//console.log("a*b=",a*b);
+//console.log("a**b=",a**b);//25
+//Asignment Operators-----
+//let a = 6;
+//let b =3;
+//a +=3; //a=a+3
+//console.log("a =",a);//9
+//a %=3;//a=a%3
+//console.log ("a=",a);
+let mode="dark";
+let color;
+if (mode==="dark") {
+    color="black";
+} else if(mode==="blue"){
+    color="blue";
+}else if (mode="pink"){
+    color="pink";
+}else{
+    color="white";
+}
+console.log(color);
+if(mode==="dark"){
+console.log(mode);
+
+}
+
+let age=16;
+if(age >=18) {
+    console.log("vote");
+} else {
+    console.log("not vote");
+}
